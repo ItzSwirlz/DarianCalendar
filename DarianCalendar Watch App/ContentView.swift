4872f48c95e28b72291f87537bce54f4bdfaabfa
Joshua Peisach <itzswirlz2020@outlook.com>
2024-06-28 16:39:26 -0400

    Handle darian dates (incorrect months and days)

--- DarianCalendar Watch App/ContentView.swift
+++ DarianCalendar Watch App/ContentView.swift
@@ -16,7 +16,14 @@ struct ContentView: View {
             let mtc = MTC()
             Text(String(mtc.hours))
             Text(String(mtc.minutes))
-            Text(String(mtc.seconds))
+            Text(String(mtc.seconds)) // FIXME: Seconds 0-9: have leading 0
+
+            Text(String(format: "Year: %.f", ((MSD() / 668.59) + 140))) // FIXME: handle years properly
+
+            // FIXME: These are probably wrong. Some months have 28 days, some have 27. Probably best to find a better way to handle this. Also, if the MSD's number is divisible by 24 it will screw everything up... so figure this all out.
+            Text(String(format: "Month No. (Wrong): %.f", (((MSD().truncatingRemainder(dividingBy: 24.0))))))
+            Text(String(format: "Sol No. (Wrong): %.f", (MSD().truncatingRemainder(dividingBy: 27.8579))))
+
         }
     }
 }
--- DarianCalendar/ContentView.swift
+++ DarianCalendar/ContentView.swift
@@ -9,14 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        `TimelineView`(AnimationTimelineSchedule()) { context in
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            var mtc = MTC()
-            Text(String(String(mtc.hours) + ":" + String(mtc.minutes) + ":" + String(mtc.seconds)))
+            let mtc = MTC()
+            Text(String(mtc.hours))
+            Text(String(mtc.minutes))
+            Text(String(mtc.seconds)) // FIXME: Seconds 0-9: have leading 0
+            
+            Text(String(format: "Year: %.f", ((MSD() / 668.59) + 140))) // FIXME: handle years properly
+            
+            // FIXME: These are probably wrong. Some months have 28 days, some have 27. Probably best to find a better way to handle this
+            Text(String(format: "Month No. (Wrong): %.f", (((MSD().truncatingRemainder(dividingBy: 24.0))))))
+            Text(String(format: "Sol No. (Wrong): %.f", (MSD().truncatingRemainder(dividingBy: 27.8579))))
         }
-        .padding()
     }
 }
 
